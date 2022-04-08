b62f65093feb8acc77adccf70f7f9be6b894a1bb
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-08 13:22:35 -0700

    profile model

--- awardx/models.py
+++ awardx/models.py
@@ -1,3 +1,23 @@
+from distutils.command.upload import upload
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    """Model definition for Profile."""
+
+    # TODO: Define fields here
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = models.ImageField(upload_to = 'awwwards/static/images')
+    bio = models.TextField()
+    link = models.URLField()
+
+    class Meta:
+        """Meta definition for Profile."""
+
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+    def __str__(self):
+        """Unicode representation of Profile."""
+        return str(self.user)
