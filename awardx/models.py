efee7cf54a4ae0d141dc386204d26faadc511cf3
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-08 13:28:57 -0700

    project model

--- awardx/models.py
+++ awardx/models.py
@@ -1,4 +1,6 @@
 from distutils.command.upload import upload
+from pyexpat import model
+from turtle import title
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,9 +10,9 @@ class Profile(models.Model):
 
     # TODO: Define fields here
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_photo = models.ImageField(upload_to = 'awwwards/static/images')
+    profile_photo = models.ImageField(upload_to = 'awwwards/static/images/profiles/')
     bio = models.TextField()
-    link = models.URLField()
+    my_link = models.URLField()
 
     class Meta:
         """Meta definition for Profile."""
@@ -21,3 +23,24 @@ class Profile(models.Model):
     def __str__(self):
         """Unicode representation of Profile."""
         return str(self.user)
+
+class Project(models.Model):
+    """Model definition for Project."""
+
+    # TODO: Define fields here
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    image = models.ImageField(upload_to = 'awwwards/static/images/projects/')
+    description = models.TextField()
+    link = models.URLField()
+
+    class Meta:
+        """Meta definition for Project."""
+
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+
+    def __str__(self):
+        """Unicode representation of Project."""
+        pass
+
