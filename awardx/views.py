c7298d5472c244a100cc273a42094af72698edf3
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-08 23:57:14 -0700

    add user projects

--- awardx/views.py
+++ awardx/views.py
@@ -10,6 +10,7 @@ def home(request):
 
 def profile(request, pk):
     user = Profile.objects.get(id = pk)
+    user_projects = user.project_set.all()
 
-    context = {'user': user, }
+    context = {'user': user, 'user_projects': user_projects,}
     return render(request, 'awardx/profile.html', context)
