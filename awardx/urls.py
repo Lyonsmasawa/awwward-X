62f88d566e3d546c8f11d8c7a1501c039fe3bdc1
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-09 04:37:02 -0700

    submit url

--- awardx/urls.py
+++ awardx/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('profile/<str:pk>', views.profile, name="profile"),
+    path('submit-site/', views.submitSite, name="submit-site"),
 ]
 
 urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
