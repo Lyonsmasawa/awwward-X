6200eed141a463722aaf19df65a3354eb08b1f5f
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-11 07:43:03 -0700

    update UI

--- awardx/models.py
+++ awardx/models.py
@@ -1,6 +1,4 @@
 from distutils.command.upload import upload
-from pyexpat import model
-from turtle import title
 from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ModelForm
