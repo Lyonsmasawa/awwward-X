c43c9b571fdd6b6d984033967a5c1c1b6a7da2ad
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-11 06:00:27 -0700

    update profile data fields

--- awardx/serializer.py
+++ awardx/serializer.py
@@ -5,11 +5,10 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Profile
-        fields = ('user', 'profile_photo',  'bio', 'my_link', 'followers', 'location', 'created',)
+        fields = ('user', 'profile_photo',  'bio', 'my_link', 'followers', 'following', 'location', 'created',)
 
 class ProjectSerializer(serializers.ModelSerializer):
     
     class Meta :
         model = Project
         fields = ('owner', 'title', 'image', 'description', 'link', 'average_design',  'average_usability', 'average_creativity', 'average_content', 'average_score',  'created',)
- 
