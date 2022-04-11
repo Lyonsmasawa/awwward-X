9dbaab47d77baa36afb3df02c608e5bbc65e4e52
Lyonsmasawa <lyonsmasawa@gmail.com>
2022-04-11 05:43:43 -0700

    create serializer filer

---
+++ awardx/serializer.py
@@ -0,0 +1,16 @@
+from dataclasses import field
+from pyexpat import model
+from rest_framework import serializers
+from .models import Profile, Project
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Profile
+        fields = ('user', 'profile_photo',  'bio', 'my_link', 'followers', 'location', 'created',)
+
+class ProjectSerializer(serializers.ModelSerializer):
+    
+    class Meta :
+        model = Project
+        fields = ('owner', 'title', 'image', 'description', 'link', 'average_design',  'average_usability', 'average_creativity', 'average_content', 'average_score',  'created',)
